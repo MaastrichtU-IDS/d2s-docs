425b1a8fdecf0b1fdce0532f56d06366bb468719
vemonet <vincent.emonet@gmail.com>
2019-09-07 20:35:22 +0200

    edit readme and add github pages deploy

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# Docusaurus documentation for the Data2Services project
+# Data2Services project documentation
+
+Generated using [Docusaurus](https://docusaurus.io/).
+
+## Development
+
+To run in the `/website` directory.
+
+```bash
+npm install
+npm start
+```
+
+## Deploy to GitHub pages
+
+To run in `/website` directory. Make sure the `/website/build` directory has been generated before deploying.
+
+```bash
+npm install
+npm run build
+GIT_USER=MaastrichtU-IDS CURRENT_BRANCH=master USE_SSH=true npm run publish-gh-pages
+```
+
+## Install boostrap tool
 
 ```bash
 sudo npm install --global yarn
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,10 +24,14 @@ const siteConfig = {
   title: 'Data2Services', // Title for your website.
   tagline: 'Automatically deploy user-facing services consuming data from various sources integrated under a common Knowledge Graph',
   url: 'https://data2services.github.io/', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  baseUrl: '/data2services/', // Base URL for your project */
+  projectName: 'data2services',  // The name of your GitHub project. Same as above.
+  organizationName: 'MaastrichtU-IDS', // Your GitHub username.
+
+  // Deploy to GitHub pages (first generate build dir)
+  // npm run build
+  // GIT_USER=<GIT_USER> CURRENT_BRANCH=master USE_SSH=true npm run publish-gh-pages
+
 
   // Used for publishing and more
   projectName: 'test-site',
@@ -68,6 +72,46 @@ const siteConfig = {
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} Institute of Data Science at Maastricht University`,
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  stylesheets: [
+    "https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700"
+  ],
+
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  ogImage: 'img/undraw_online.svg',
+  twitterImage: 'img/undraw_tweetstorm.svg',
+  twitterUsername: 'um_ids',
+
+  // For sites with a sizable amount of content, set collapsible to true.
+  // Expand/collapse the links and subcategories under categories.
+  docsSideNavCollapsible: false,
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+
+  /* Custom fonts for website. Not working. Directly in custom.css */
+  // fonts: {
+  //   myFont: [
+  //     "Open Sans",
+  //     "Roboto",
+  //     "Serif"
+  //   ]
+  // },
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
@@ -239,46 +283,6 @@ const siteConfig = {
       }      
     )
   }},
-
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
-
-  stylesheets: [
-    "https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700"
-  ],
-
-  // On page navigation for the current documentation page.
-  onPageNav: 'separate',
-  // No .html extensions for paths.
-  cleanUrl: true,
-
-  // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
-  twitterUsername: 'um_ids',
-
-  // For sites with a sizable amount of content, set collapsible to true.
-  // Expand/collapse the links and subcategories under categories.
-  docsSideNavCollapsible: false,
-
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
-
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
-
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
-
-  /* Custom fonts for website. Not working. Directly in custom.css */
-  // fonts: {
-  //   myFont: [
-  //     "Open Sans",
-  //     "Roboto",
-  //     "Serif"
-  //   ]
-  // },
 };
 
 module.exports = siteConfig;
