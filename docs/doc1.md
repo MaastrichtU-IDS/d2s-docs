0a7750aaef2a3e487b10b05e5e19469ad2994d0b
vemonet <vincent.emonet@gmail.com>
2019-09-07 17:39:09 +0200

    first getstarted page and try to add sparql and ttl highlight

--- docs/doc1.md
+++ docs/doc1.md
@@ -1,18 +1,29 @@
 ---
-id: doc1
-title: Latin-ish
+id: getstarted
+title: Get started
 sidebar_label: Example Page
 ---
 
-Check the [documentation](https://docusaurus.io) for how to use Docusaurus.
+This is a demonstrator ETL pipeline that **converts** relational databases, tabular files, and XML files **to RDF**. A generic RDF, based on the input data structure, is generated and [SPARQL](https://www.w3.org/TR/sparql11-query/) queries are **designed by the user** to map the generic RDF to a **specific model**.
 
-## Lorem
+* Only [Docker](https://docs.docker.com/install/) is required to run the pipeline. Checkout the [Wiki](https://github.com/MaastrichtU-IDS/data2services-pipeline/wiki/Docker-documentation) if you have issues with Docker installation.
+* Following documentation **focuses on Linux & MacOS**.
+* **Windows documentation** can be found [here](https://github.com/MaastrichtU-IDS/data2services-pipeline/wiki/Run-on-Windows).
+* Modules are from the [Data2Services ecosystem](https://github.com/MaastrichtU-IDS/data2services-ecosystem). 
+* See [data2services-transform-biolink](https://github.com/MaastrichtU-IDS/data2services-transform-biolink) to run Data2Services transformation workflows using [CWL](https://www.commonwl.org/) or [Argo](https://argoproj.github.io/argo/).
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum massa eget nulla aliquet sagittis. Proin odio tortor, vulputate ut odio in, ultrices ultricies augue. Cras ornare ultrices lorem malesuada iaculis. Etiam sit amet libero tempor, pulvinar mauris sed, sollicitudin sapien.
+---
 
-## Mauris In Code
+## The Data2Services philosophy
 
-```sql
+Docker containers running with a few parameters (e.g. input file path, SPARQL endpoint, credentials, mapping file path)
+
+- **Build** or pull the Docker images.
+- **Start required services** ([Apache Drill](https://github.com/amalic/apache-drill) and [GraphDB](https://github.com/MaastrichtU-IDS/graphdb)).
+- **Execute the Docker modules** you want, providing the proper parameters.
+
+
+```sparql
 INSERT {
   GRAPH <?_output> {
     ?Concept a <https://w3id.org/data2services/Concept> .
@@ -25,15 +36,3 @@ INSERT {
       } LIMIT 10 
 } } }
 ```
-
-## Nulla
-
-Nulla facilisi. Maecenas sodales nec purus eget posuere. Sed sapien quam, pretium a risus in, porttitor dapibus erat. Sed sit amet fringilla ipsum, eget iaculis augue. Integer sollicitudin tortor quis ultricies aliquam. Suspendisse fringilla nunc in tellus cursus, at placerat tellus scelerisque. Sed tempus elit a sollicitudin rhoncus. Nulla facilisi. Morbi nec dolor dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras et aliquet lectus. Pellentesque sit amet eros nisi. Quisque ac sapien in sapien congue accumsan. Nullam in posuere ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin lacinia leo a nibh fringilla pharetra.
-
-## Orci
-
-Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin venenatis lectus dui, vel ultrices ante bibendum hendrerit. Aenean egestas feugiat dui id hendrerit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur in tellus laoreet, eleifend nunc id, viverra leo. Proin vulputate non dolor vel vulputate. Curabitur pretium lobortis felis, sit amet finibus lorem suscipit ut. Sed non mollis risus. Duis sagittis, mi in euismod tincidunt, nunc mauris vestibulum urna, at euismod est elit quis erat. Phasellus accumsan vitae neque eu placerat. In elementum arcu nec tellus imperdiet, eget maximus nulla sodales. Curabitur eu sapien eget nisl sodales fermentum.
-
-## Phasellus
-
-Phasellus pulvinar ex id commodo imperdiet. Praesent odio nibh, sollicitudin sit amet faucibus id, placerat at metus. Donec vitae eros vitae tortor hendrerit finibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque vitae purus dolor. Duis suscipit ac nulla et finibus. Phasellus ac sem sed dui dictum gravida. Phasellus eleifend vestibulum facilisis. Integer pharetra nec enim vitae mattis. Duis auctor, lectus quis condimentum bibendum, nunc dolor aliquam massa, id bibendum orci velit quis magna. Ut volutpat nulla nunc, sed interdum magna condimentum non. Sed urna metus, scelerisque vitae consectetur a, feugiat quis magna. Donec dignissim ornare nisl, eget tempor risus malesuada quis.
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,7 +38,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
+    {doc: 'getstarted', label: 'Docs'},
     // {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     // {blog: true, label: 'Blog'},
@@ -64,8 +64,174 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
+    // TODO: add SPARQL
     theme: 'default',
-  },
+    hljs: function(hljs) {
+      hljs.registerLanguage('ttl',function(hljs) {
+        var KEYWORDS = {
+          keyword: 'base|10 prefix|10 @base|10 @prefix|10',
+          literal: 'true|0 false|0',
+          built_in: 'a|0'
+        };
+      
+        var IRI_LITERAL = {// https://www.w3.org/TR/turtle/#grammar-production-IRIREF
+          className: 'literal',
+          relevance: 1, // XML tags look also like relative IRIs
+          begin: /</,
+          end: />/,
+          illegal: /[^\x00-\x20<>"{}|^`]/, // TODO: https://www.w3.org/TR/turtle/#grammar-production-UCHAR
+        };
+        
+        // https://www.w3.org/TR/turtle/#terminals
+        var PN_CHARS_BASE    = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u10000-\uEFFFF';
+        var PN_CHARS_U       = PN_CHARS_BASE+'_';
+        var PN_CHARS         = '-'+PN_CHARS_U+'0-9\u00B7\u0300-\u036F\u203F-\u2040';
+        var BLANK_NODE_LABEL = '_:['+PN_CHARS_U+'0-9](['+PN_CHARS+'.]*['+PN_CHARS+'])?';
+        var PN_PREFIX        = '['+PN_CHARS_BASE+'](['+PN_CHARS+'.]*['+PN_CHARS+'])?';
+        var PERCENT          = '%[0-9A-Fa-f][0-9A-Fa-f]';
+        var PN_LOCAL_ESC     = '\\\\[_~.!$&\'()*+,;=/?#@%-]';
+        var PLX              = PERCENT+'|'+PN_LOCAL_ESC;
+        var PNAME_NS         = '('+PN_PREFIX+')?:';
+        var PN_LOCAL         = '(['+PN_CHARS_U+':0-9]|'+PLX+')(['+PN_CHARS+'.:]|'+PLX+')*(['+PN_CHARS+':]|'+PLX+')?';
+        var PNAME_LN         = PNAME_NS+PN_LOCAL;
+        var PNAME_NS_or_LN   = PNAME_NS+'('+PN_LOCAL+')?';
+        
+        var PNAME = {
+          begin: PNAME_NS_or_LN,
+          relevance: 0,
+          className: 'symbol',
+        };
+      
+        var BLANK_NODE = {
+          begin: BLANK_NODE_LABEL,
+          relevance: 10,
+          className: 'template-variable',
+        };
+      
+        var LANGTAG = {
+          begin: /@[a-zA-Z]+([a-zA-Z0-9-]+)*/,
+          className: 'type',
+          relevance: 5, // also catches objectivec keywords like: @protocol, @optional
+        };
+      
+        var DATATYPE =  {
+          begin: '\\^\\^'+PNAME_LN,
+          className: 'type',
+          relevance: 10,
+        };
+      
+        var TRIPLE_APOS_STRING = {
+          begin: /'''/,
+          end: /'''/,
+          className: 'string',
+          relevance: 0,
+        };
+      
+        var TRIPLE_QUOTE_STRING = {
+          begin: /"""/,
+          end: /"""/,
+          className: 'string',
+          relevance: 0,
+        };
+        
+        var APOS_STRING_LITERAL = JSON.parse(JSON.stringify(hljs.APOS_STRING_MODE));
+        APOS_STRING_LITERAL.relevance = 0;
+      
+        var QUOTE_STRING_LITERAL = JSON.parse(JSON.stringify(hljs.QUOTE_STRING_MODE));
+        QUOTE_STRING_LITERAL.relevance = 0;
+      
+        var NUMBER = JSON.parse(JSON.stringify(hljs.C_NUMBER_MODE));
+        NUMBER.relevance = 0;
+      
+        return {
+          case_insensitive: true,
+          keywords: KEYWORDS,
+          aliases: ['turtle', 'n3'],
+          contains: [
+            LANGTAG,
+            DATATYPE,
+            IRI_LITERAL,
+            BLANK_NODE,
+            PNAME,
+            TRIPLE_APOS_STRING, TRIPLE_QUOTE_STRING, // order matters
+            APOS_STRING_LITERAL, QUOTE_STRING_LITERAL,
+            NUMBER,
+            hljs.HASH_COMMENT_MODE,
+          ],
+          exports: {
+            LANGTAG: LANGTAG,
+            DATATYPE: DATATYPE,
+            IRI_LITERAL: IRI_LITERAL,
+            BLANK_NODE: BLANK_NODE,
+            PNAME: PNAME,
+            TRIPLE_APOS_STRING: TRIPLE_APOS_STRING,
+            TRIPLE_QUOTE_STRING: TRIPLE_QUOTE_STRING,
+            APOS_STRING_LITERAL: APOS_STRING_LITERAL,
+            QUOTE_STRING_LITERAL: QUOTE_STRING_LITERAL,
+            NUMBER: NUMBER,
+            KEYWORDS: KEYWORDS,
+          }
+        };
+      }      
+    );
+      hljs.registerLanguage("sparql",
+      function(hljs) {
+        var ttl = hljs.getLanguage('ttl').exports;
+        var KEYWORDS = {
+          keyword: 'base|10 prefix|10 @base|10 @prefix|10 add all as|0 ask bind by|0 clear construct|10 copymove create data default define delete describe distinct drop exists filter from|0 graph|10 group having in|0 insert limit load minus named|10 not offset optional order reduced select|0 service silent to union using values where with|0',
+          function: 'abs asc avg bound ceil coalesce concat containsstrbefore count dayhours desc encode_for_uri floor group_concat if|0 iri isblank isiri isliteral isnumeric isuri langdatatype langmatches lcase max md5 min|0 minutes month now rand regex replace round sameterm sample seconds separator sha1 sha256 sha384 sha512 str strafter strdt strends strlang strlen strstarts struuid substr sum then timezone tz ucase uribnode uuid year',
+          literal: 'true|0 false|0',
+          built_in: 'a|0'
+        };
+      
+        var VARIABLE = {
+          className: 'variable',
+          begin: '[?$]' + hljs.IDENT_RE,
+          relevance: 0,
+        };
+      
+        var JSON_QUOTE_STRING = {
+          begin: /"""\s*\{/,          // TODO why can't I write (?=\{)
+          end: /"""/,
+          subLanguage: 'json',
+          excludeBegin: true,
+          excludeEnd: true,
+          relevance: 0,
+        };
+        
+        var JSON_APOS_STRING = {
+          begin: /'''\s*\{/,          // TODO why can't I write (?=\{)
+          end: /'''/,
+          subLanguage: 'json',
+          excludeBegin: true,
+          excludeEnd: true,
+          relevance: 0,
+        };
+        
+        return {
+          case_insensitive: true,
+          keywords: KEYWORDS,
+          aliases: ['rql', 'rq', 'ru'],
+          contains: [
+            ttl.LANGTAG,
+            ttl.DATATYPE,
+            ttl.IRI_LITERAL,
+            ttl.BLANK_NODE,
+            ttl.PNAME,
+            VARIABLE,
+            JSON_QUOTE_STRING, // order matters
+            JSON_APOS_STRING,
+            ttl.TRIPLE_QUOTE_STRING,
+            ttl.TRIPLE_APOS_STRING,
+            ttl.QUOTE_STRING_LITERAL,
+            ttl.APOS_STRING_LITERAL,
+            ttl.NUMBER,
+            hljs.HASH_COMMENT_MODE,
+          ]
+        };
+      }      
+    )
+  }},
 
   // Add custom scripts here that would be placed in <script> tags.
   scripts: ['https://buttons.github.io/buttons.js'],
