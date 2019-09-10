d6be069cb3d350543e69ffdf736e5b7dcdc5924a
vemonet <vincent.emonet@gmail.com>
2019-09-10 10:31:40 +0200

    add logo to homepage

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -60,7 +60,7 @@ class HomeSplash extends React.Component {
 
     return (
       <SplashContainer>
-        {/* <Logo img_src={`${baseUrl}img/favicon.ico`} /> */}
+        <Logo img_src={`${baseUrl}img/d2s-logo.png`} />
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
           <PromoSection>
